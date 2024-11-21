ff1fa430b9134cef00e7b18039f53c634e524669
2213amanda <97637450+2213amanda@users.noreply.github.com>
2024-11-21 20:17:51 +0000

    added init stuffs

--- tests/test-lab.c
+++ tests/test-lab.c
@@ -1,4 +1,11 @@
-#include "harness/unity.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+#ifdef __APPLE__
+#include <sys/errno.h>
+#else
+#include <errno.h>
+#endif
 #include "../src/lab.h"
 
 
@@ -10,12 +17,34 @@ void tearDown(void) {
   // clean stuff up here
 }
 
-void test_stuff(void){
-  // test stuff here
+/**
+ * Tests to make sure that the struct buddy_pool is correct and all fields
+ * have been properly set kval_m, avail[kval_m], and base pointer after a
+ * call to init
+ */
+void test_buddy_init(void)
+{
+  perror("->Testing buddy init\n");
+  //Loop through all kval MIN_k-DEFAULT_K and make sure we get the correct amount allocated.
+  //We will check all the pointer offsets to ensure the pool is all configured correctly
+  for (size_t i = MIN_K; i <= DEFAULT_K; i++)
+    {
+      size_t size = UINT64_C(1) << i;
+      struct buddy_pool pool;
+      buddy_init(&pool, size);
+      check_buddy_pool_full(&pool);
+      buddy_destroy(&pool);
+    }
 }
 
 int main(void) {
+  time_t t;
+  unsigned seed = (unsigned)time(&t);
+  perror("Random seed:%d\n", seed);
+  srand(seed);
+  printf("Running memory tests.\n");
+
   UNITY_BEGIN();
-  RUN_TEST(test_stuff);
-  return UNITY_END();
+  RUN_TEST(test_buddy_init);
+return UNITY_END();
 }
